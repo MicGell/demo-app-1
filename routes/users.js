213902c25fb7025b60658364f69ba525a949eb9d
miczal <miczal19@gamil.com>
2017-08-30 16:36:43 +0200

    create User Page Comments: ejs, css

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
-
 router.get("/", function(req, res){
-    res.render("users/allShow");
+    res.render("users/allShow", {stylesheetPage: "userPageComments.css"});
 });
 router.get("/:id", function(req, res){
-    res.render("users/singleShow");
+    res.render("users/singleShow", {stylesheetPage: "userPageComments.css"});
 });
 
 
