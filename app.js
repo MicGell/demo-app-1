eedd93f7dd3c98ad7864b02957e7de791a1c6f21
miczal <miczal19@gamil.com>
2017-08-30 16:42:52 +0200

    add body-parser, separated userPageComments.css from main.css

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var express = require("express"),
-    app = express();
+var express 		= require("express"),
+	bodyParser      = require("body-parser"),
+    app 			= express();
     
 //requring routes
 var usersRoutes = require("./routes/users"),
     indexRoutes = require("./routes/index");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
