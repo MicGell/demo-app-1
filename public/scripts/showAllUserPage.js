eece3826af55604cb17320eaac4cdd384bd49954
miczal <miczal19@gmail.com>
2017-09-02 19:38:40 +0200

    correct links on clicks to profile users

--- public/scripts/showAllUserPage.js
+++ public/scripts/showAllUserPage.js
@@ -1,10 +1,12 @@
 var informationOfUserContainer = document.querySelectorAll(".informationOfUser");
-var informationOfUserLink = document.querySelectorAll(".informationOfUser a");
+var informationOfUserLink = document.querySelectorAll(".currentLocation");
 
 init();
 
 function init(){
 	informationOfUserContainer.forEach( function(element, index) {
+		console.log(index);
+		console.log(element);
 		element.addEventListener("click", function () {
 			window.location.replace(informationOfUserLink[index].href);
 		});
